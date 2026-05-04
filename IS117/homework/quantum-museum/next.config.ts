d3ec2e582524c7cebc5dd27f8194e1255897b175
XxCaptainNinjaxX <captainninjaflash@gmail.com>
2026-05-04 15:06:38 -0400

    .

--- IS117/homework/quantum-museum/next.config.ts
+++ IS117/homework/quantum-museum/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "export",
-    basePath: "/Classwork.homework",
+    basePath: "/quantum-museum",
     images: {
         unoptimized: true,
     },
